ef42a4677a7526fb480dc5290976a9fe310b74e4
DineshR <dineshranasinghe@gmail.com>
2016-06-23 20:20:21 +1000

    Production ready code is added

--- assets/app/boot.ts
+++ assets/app/boot.ts
@@ -2,9 +2,11 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { ROUTER_PROVIDERS } from "@angular/router";
 import { LocationStrategy, HashLocationStrategy } from "@angular/common";
-import { provide } from "@angular/core";
+import { provide,  enableProdMode} from "@angular/core";
 
 
 import { AppComponent } from "./app.component";
+
+enableProdMode();
 
 bootstrap(AppComponent, [ROUTER_PROVIDERS, provide(LocationStrategy, {useClass: HashLocationStrategy})]);
