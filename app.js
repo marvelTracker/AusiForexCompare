7955ba82a53caa1885ccd6a589bbbf46a3603de5
DineshR <dineshranasinghe@gmail.com>
2016-06-23 20:04:44 +1000

    Disable mongo db

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var appRoutes = require('./routes/app');
 //var currencyRoutes = require('./routes/currency')
 
 var app = express();
-mongoose.connect('localhost:27017/node-angular');
+//mongoose.connect('localhost:27017/node-angular');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
